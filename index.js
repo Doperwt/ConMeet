60b34494e48f6673b6106a04474393cb42565ac2
Robert den Ouden <r.f.denouden@gmail.com>
2018-02-09 13:27:11 +0100

    reworked backend

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var Meetup = require('meetup')
 var mup = new Meetup()
+var app = require('express')()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+
+server.listen(3002)
 
 let topicsCounter = {}
+let arrayOfTopicsWithCount = {}
+console.log('no connection')
 
 mup.stream('/2/rsvps', stream => {
   stream
   .on('data', item => {
-    console.log('got item ' + item)
+    console.log('got event ' + item.event.event_name)
 
     // inside of our stream event handler (!) we retrieve the group topics
     const topicNames = item.group.group_topics.map(topic => topic.topic_name)
+    if(!topicNames.includes('Software Development')) return
     topicNames.forEach(name => {
       if (topicsCounter[name]) {
         topicsCounter[name]++
@@ -30,8 +38,17 @@ mup.stream('/2/rsvps', stream => {
         return 0
       }
     })
-    console.log(arrayOfTopics.slice(0,10))
+
+    let top10 = arrayOfTopics.slice(0,10)
+    arrayOfTopicsWithCount = top10.map(topic =>   { return {topic:topic,count:topicsCounter[topic]}})
+    console.log('update top10')
+    io.emit('action', arrayOfTopicsWithCount)
   }).on('error', e => {
-    console.log('error! ' + e)
-  });
-});
+    io.emit('error! ' + e)
+  })
+})
+
+io.on('connection', socket => {
+  socket.emit('action',arrayOfTopicsWithCount)
+  console.log('got connection')
+})
